f658c65a3634995a9cb082371cfaa4a7e25607fa
yji00 <dksro333@naver.com>
2021-10-15 03:20:15 +0900

    home components 만드는 중

---
+++ src/components/Search.js
@@ -0,0 +1,28 @@
+import React, { useCallback, useRef } from 'react';
+import styled from 'styled-components'
+
+const Form = styled.div`
+  max-width: 80%;
+  margin: auto;
+`
+
+const Search = () => {
+
+  const queryRef =useRef('')
+  const onQuery = useCallback(e => {
+    e.prevnetDefault()
+  }, [])
+
+  return (
+    <Form>
+      <form onSubmit={ onQuery } className="input-group cherry">
+        <input type="text" rel={ queryRef } className="form-control border-danger" name="query" placeholder="검색어를 입력하세요." />
+        <div className="input-group-append">
+        <button className="btn btn-danger">검색</button>
+        </div>
+      </form>
+    </Form>
+  );
+}
+
+export default Search;
