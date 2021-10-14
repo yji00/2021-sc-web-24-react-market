ad6fa20e287619b78f49b05e1a5fc7cb00df9a88
yji00 <dksro333@naver.com>
2021-10-15 02:02:30 +0900

    store 고도화 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import { getWebAction } from './store/actions/web-action' //비동기 액션
-import { actQuery, getData} from './store/reducers/webReducer' //동기 액션, custom action
+import { getWebData } from './store/reducers/webReducer' //동기 액션, custom action
 
 
 function App() {
@@ -10,7 +9,8 @@ function App() {
 	const { query, isQuering, isEnd, pageCnt, listCnt, lists } = useSelector(state => state) //state
 
 	const onQuery = useCallback((e) => {
-		dispatch(getData('react', 10, ['web', 'Clip', 'img', 'blog', 'book']))
+		// dispatch(getWebData('react', 10, ['web', 'Clip', 'img', 'blog', 'book']))
+		dispatch(getWebData('react', 10))
 	},[dispatch])
 
 	return (
