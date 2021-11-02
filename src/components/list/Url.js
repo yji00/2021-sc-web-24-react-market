d1641941e8caee47b633e1997d240a0eea5ac6e7
yji00 <dksro333@naver.com>
2021-11-02 18:08:45 +0900

    component 만들기 시작

---
+++ src/components/list/Url.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components'
+import { color, media, font } from '../styled/variables'
+
+const List = styled.div`
+
+`
+
+const Title = styled.h3`
+
+`
+
+const Content = styled.p`
+
+`
+
+const Weblist = () => {
+  return (
+    <List>
+      <Title></Title>
+      <Content></Content>
+    </List>
+  );
+}
+
+export default Weblist;
